8dec3eed2dd6866aac9c0a89a7fe89d4d133a984
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-29 09:39:39 +0000

    [Finish] Application-task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
 
   def show
+    @bookmake = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -21,6 +24,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
+    if current_user != @book.user
+    redirect_to books_path
+    end
   end
 
   def update
@@ -32,15 +39,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
