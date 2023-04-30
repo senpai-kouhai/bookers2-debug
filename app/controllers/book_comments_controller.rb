6ec2afe18fc17e66493686833c9f4ce20ee3a6e6
senpai-kouhai <129678369+senpai-kouhai@users.noreply.github.com>
2023-04-30 09:42:21 +0000

    [Finish] Application-Task3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,13 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    redirect_to book_path(book)
+    comment.save
+    redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
 
   end
 
